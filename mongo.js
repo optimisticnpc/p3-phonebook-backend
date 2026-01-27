529adb999fd6c53d2b5a647d97b0d0d8493f5fd8
optimisticnpc <joshuajltan@gmail.com>
2026-01-27 13:18:11 +1300

    Finished 3.12 locally

---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+  console.log('give password and person details as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name= process.argv[3]
+const number= process.argv[4]
+
+// const url = `mongodb+srv://fullstack:${password}@cluster0.vr2pg.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+// mongodb+srv://<db_username>:<db_password>@cluster0.vr2pg.mongodb.net/?appName=Cluster0
+
+
+const url = `mongodb://localhost:27017/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+  name: name,
+  number: number,
+})
+
+person.save().then(result => {
+  console.log('person saved!')
+  mongoose.connection.close()
+})
