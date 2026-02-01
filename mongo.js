441751c055ba068732534172041bed451f6d8e3d
optimisitcnpc <joshuajltan@gmail.com>
2026-02-01 15:00:43 +1300

    Actually fix mongo.js

--- mongo.js
+++ mongo.js
@@ -35,14 +35,14 @@ if (process.argv.length > 3) {
   })
 
   person.save().then(() => {
-    console.log('person saved!')
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+} else {
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log(person)
+    })
     mongoose.connection.close()
   })
 }
-
-Person.find({}).then(result => {
-  result.forEach(person => {
-    console.log(person)
-  })
-  mongoose.connection.close()
-})
