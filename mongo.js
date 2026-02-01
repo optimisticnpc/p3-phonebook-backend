683348c973b329aa3e41e2b26b3a1b982b892655
optimisitcnpc <joshuajltan@gmail.com>
2026-02-01 14:56:08 +1300

    Actually fix mongo.js for 3.12

--- mongo.js
+++ mongo.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 5) {
-  console.log('give password and person details as argument')
+if (process.argv.length < 3) {
+  console.log('give password as argument')
   process.exit(1)
 }
 
 const password = process.argv[2]
-const name= process.argv[3]
-const number= process.argv[4]
 
 const url = `mongodb+srv://fullstack:${password}@cluster0.vr2pg.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
 
@@ -27,19 +25,24 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-  name: name,
-  number: number,
-})
+if (process.argv.length > 3) {
+  const name= process.argv[3]
+  const number= process.argv[4]
 
-person.save().then(() => {
-  console.log('person saved!')
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(() => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+}
+
+Person.find({}).then(result => {
+  result.forEach(person => {
+    console.log(person)
+  })
   mongoose.connection.close()
 })
-
-// Person.find({}).then(result => {
-//   result.forEach(person => {
-//     console.log(person)
-//   })
-//   mongoose.connection.close()
-// })
